0fa5cc68e9bce5e5a36e71905c5df8ffd374bed2
Clavin June <juneardoc@gmail.com>
2021-10-21 07:44:53 +0700

    add copy func
    Signed-off-by: Clavin June <juneardoc@gmail.com>

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,3 +1,27 @@
 for (const e of document.querySelectorAll('div.highlight')) {
-    e.innerHTML += `<div class="copy-button"><span class="font-16 fg-bold bottom-red">copy</span></div>`
+    e.innerHTML += `<div class="copy-button"><span id="copy" class="font-16 fg-bold bottom-red">copy</span></div>`
 }
+
+const copy = text => {
+    if (navigator.clipboard && window.isSecureContext) {
+        return navigator.clipboard.writeText(text)
+    }
+
+    let temp = document.createElement("textarea")
+    temp.value = text
+    document.body.appendChild(temp)
+    temp.select()
+    document.execCommand("copy")
+    document.body.removeChild(temp)
+}
+
+document.addEventListener('click', e => {
+    if (e.target && e.target.id === 'copy') {
+        copy(e.
+            target.
+            parentElement.
+            previousElementSibling.
+            querySelector("table > tbody > tr > :nth-child(2)").
+            textContent.trim())
+    }
+  })
